d1d5be3a9c33173b7a6ed0ee6625a9bd18010cd1
wipphil <84144261+wipphil@users.noreply.github.com>
2021-10-11 14:59:36 -0500

    Updated home page, header, and footer

--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -4,32 +4,33 @@ import styles from './HomepageFeatures.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
-    Svg: require('../../static/img/undraw_docusaurus_mountain.svg').default,
+    title: 'Metaverse Writers Guild',
+    Svg: require('../../static/img/undraw_Working.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Our community is comprised of writers, readers and all creatives working to bring literature to blockchain. 
+        <div class="button"><a href = "https://discord.com/invite/MTWhQjnMVh">Join the discord</a></div>
+      </>
+    ),
+    
+  },
+  {
+    title: 'PAGE',
+    Svg: require('../../static/img/undraw_transfer_money.svg').default,
+    description: (
+      <>
+        Our $PAGE utility token serves as a currency within the PAGE Network to reward literary creatives and supporters for their contributions in the DAO.
+        <div class="button"><a href = "https://v2.info.uniswap.org/pair/0x9a25d21e204f10177738edb0c3345bd88478aaa2">Trade on Uniswap</a></div>
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('../../static/img/undraw_docusaurus_tree.svg').default,
+    title: 'DAO',
+    Svg: require('../../static/img/undraw_conference_speaker.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
-  },
-  {
-    title: 'Powered by React',
-    Svg: require('../../static/img/undraw_docusaurus_react.svg').default,
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Our decentralized autonomous organization is self-governing and works collectively to further our vision to bring Web 3.0 literature to life.
+        <div class="button"><a href = "https://vote.pagedao.org/#/pagedao.eth">Snapshot Governance</a></div>
       </>
     ),
   },
