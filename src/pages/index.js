b3af83b18f17c65ddec4a16a67f2b7b6b1953397
ipfsnut <40326908+ipfsnut@users.noreply.github.com>
2022-10-05 13:43:00 -0500

    update HomepageHeader
    redirected to /docs directory

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/whitepaper/intro">
+            to="/docs">
             Read the Whitepaper
           </Link>
         </div>
