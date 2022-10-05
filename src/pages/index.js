d86d04c718bb6d856ddef76aad36b43da1e13631
ipfsnut <40326908+ipfsnut@users.noreply.github.com>
2022-10-05 13:44:14 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs">
-            Read the Whitepaper
+            Read Our Docs
           </Link>
         </div>
       </div>
