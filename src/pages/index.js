aef53d4a18a130f2cedc38113b51430c7fbc360f
ipfsnut <40326908+ipfsnut@users.noreply.github.com>
2022-10-05 16:34:55 -0500

    returned whitepaper uri
    we'll see if this works

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,8 +16,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs">
-            Read Our Docs
+            to="/docs/whitepaper/intro">
+            Read The Docs
           </Link>
         </div>
       </div>
