feea7e6e8053d1699c5e2b4d246229cf8af9014e
ipfsnut <40326908+ipfsnut@users.noreply.github.com>
2022-10-06 02:23:35 -0500

    Updated links in main section to be more clear
    just memberships, uniswap, and Dework! our killer apps.

--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -9,10 +9,7 @@ const FeatureList = [
     description: (
       <>
         Our community is comprised of writers, readers and all creatives working to bring permissionless decentralized literature to Web3. 
-        <br></br><div class="button"><a href = "https://bit.ly/pagedao-discord">Join PageDAO Discord</a></div>
-        <br></br><div class="button"><a href = "https://page.cent.co">Follow our Cent Page for email updates!</a></div>
-        <br></br><div class="button"><a href = "https://mint.nftbookbazaar.com">Mint your book to the Polygon blockchain</a></div>
-
+        <br></br><div class="button"><a href = "https://app.dework.xyz/pagedao-web3-writers">PageDAO on Dework</a></div>
       </>
     ),
     
@@ -24,7 +21,7 @@ const FeatureList = [
       <>
         Welcome to $PAGE, the writer's cryptocurrency. $PAGE grants govenance weight, access to DAO member perks, and can be used to pay for writing on our Dework Board.
         <br></br><div class="button"><a href = "https://app.uniswap.org/#/swap?inputCurrency=ETH&outputCurrency=0x60e683c6514edd5f758a55b6f393bebbafaa8d5e">Trade on Uniswap</a></div>
-        <br></br><div class="button"><a href = "https://app.dework.xyz/pagedao-web3-writers">PageDAO on Dework</a></div>
+
 
       </>
     ),
@@ -35,9 +32,7 @@ const FeatureList = [
     description: (
       <>
         Our decentralized autonomous organization is self-governed by $PAGE and the Membership NFTs. We work collectively to bring Web 3.0 literature to life.
-        <br></br><div class="button"><a href = "https://vote.pagedao.org/#/pagedao.eth">Snapshot Governance</a></div>
         <br></br><div class="button"><a href = "https://membership.nftbookbazaar.com">Mint Your Membership NFT</a></div>
-        <br></br><div class="button"><a href = "https://opensea.io/collection/readme-books">ReadMe Books NFTBooks on Opensea</a></div>
 
       </>
     ),
