8beac9fd77243472e8c165395c8c77811cf35603
ipfsnut <40326908+ipfsnut@users.noreply.github.com>
2022-10-06 04:49:58 -0500

    took docs section out of footer, works best in header

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -87,32 +87,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
       footer: {
         style: 'dark',
         links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'PageDAO: Salve for Authors',
-                to: '/docs/salve',
-              },
-              {
-                label: 'PageDAO First Anniversary',
-                to: 'https://ipfs.nftbookbazaar.com/ipfs/QmUQPhzfGcYjGeoJ1Dsz8bmDLWR6oDX1LmEFvvmdCd7JHF',
-              },
-              {
-                label: 'Web3 Social Media: PageDAO',
-                to: 'https://ipfs.nftbookbazaar.com/ipfs/QmXgXtcUUhc2XNp2jaiGNHfTr9XJKDR8hyBktFGvLfW87D',
-              },
-              {
-                label: 'First Annual Report',
-                to: 'https://ipfs.nftbookbazaar.com/ipfs/QmSe8k6mHBseLd8DQxDHH4CBuSXztgpyVi6NoWfxf2eUug',
-              },
-              {
-                label: 'Whitepaper',
-                to: '/docs/whitepaper',
-              },
 
-            ],
-          },
           {
             title: 'Community',
             items: [
