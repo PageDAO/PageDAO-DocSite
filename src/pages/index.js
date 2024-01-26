b50012aac045b52aafd3d3f3734442eb5de2bb93
indefatigable <43445728+rbpollock@users.noreply.github.com>
2024-01-26 17:02:40 +0000

    updated styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,7 +25,7 @@ function HomepageHeader() {
         </div>
 
         <div class="row">
-        <div className={styles.card} class="card">
+        <div class="card">
           <div class="card__header">
             <h3>New Bridging Guide</h3>
           </div>
